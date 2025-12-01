5d6da65d044ae87dd6ecee74b90ca3fd9c754c82
Briana Davis <brianad570@gmail.com>
2025-12-01 09:27:36 -0500

    Initial Commit

--- src/Geometry.Presentation/CubeApi/Controllers/CubeController.cs
+++ src/Geometry.Presentation/CubeApi/Controllers/CubeController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Geometry.Presentation.CubeApi.Controllers;
 
+
 /// <summary>
 /// Controller for managing cube operations via REST API.
 /// Provides endpoints for creating and retrieving cube entities.
